a476bc136cb5ee3fee11195072da3eef1f259559
fahimrimon <fahimrimon21@gmail.com>
2023-08-30 00:49:42 +0600

    Navbar issue solve

--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { AiOutlineArrowRight } from 'react-icons/ai';
+
 const Navbar = () => {
 
   const menuItems = (
     <>
-      <li>
+      <li className="text-[14px] font-bold text-gray-500">
         <a href="/">Home</a>
       </li>
-      <li>
+      <li className="text-[14px] font-bold text-gray-500">
         <a href="/">Services</a>
       </li>
-      <li>
+      <li className="text-[14px] font-bold text-gray-500">
         <a href="/">Pricing</a>
       </li>
-      <li>
+      <li className="text-[14px] font-bold text-gray-500">
         <a href="/">Contact</a>
       </li>
     </>
@@ -20,7 +22,7 @@ const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-white pt-4">
-      <div className="navbar-start lg:pl-[200px]">
+      <div className="navbar-start lg:pl-[200px] lg:py-6">
         <div className="dropdown">
           <label tabIndex="0" className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -36,13 +38,13 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex">
         <ul className="text-black menu menu-horizontal p-0">{menuItems}</ul>
       </div>
-      <div className="navbar-end hidden lg:flex lg:pr-[200px]">
+      <div className="navbar-end  lg:pr-[200px]">
         <ul className="font-bold menu menu-horizontal p-0">
-        <li className="text-sky-500">
+        <li className="text-sky-500 text-[14px] pr-6">
         <a href="/">Login</a>
       </li>
-        <li className="text-white bg-sky-500 rounded">
-        <a href="/">Get a qoute {"->"}</a>
+        <li className="text-white text-[14px] font-bold bg-sky-500 hover:text-white rounded">
+        <a href="/">Get a qoute <AiOutlineArrowRight /></a>
       </li>
         </ul>
       </div>
